3cda6f8aa07e3ca55cc63bce660de5b3ed0daab0
shoyonp <shoyonkumar143@gmail.com>
2024-11-15 04:51:39 +0600

    main sec running

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,17 +1,24 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
   return (
-    <div>
+    <div className="font-poppins">
       <header>
         <Header></Header>
         <section className="w-11/12 mx-auto">
-            <LatestNews></LatestNews>
+          <LatestNews></LatestNews>
         </section>
       </header>
-      <nav></nav>
-      <main></main>
+      <nav className="w-11/12 mx-auto py-2">
+        <Navbar></Navbar>
+      </nav>
+      <main className="w-11/12 mx-auto p-t-5 grid md:grid-cols-12 gap-3">
+        <aside className="left col-span-2">left number</aside>
+        <section className="col-span-6">main cintent</section>
+        <aside className="col-span-3">rignt navbar</aside>
+      </main>
     </div>
   );
 };
