f7a0ad39a48766982f7211ad54a7e08ca8e79660
shoyonp <shoyonkumar143@gmail.com>
2024-11-15 15:25:45 +0600

    login and registration from done

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const AuthLayout = () => {
     return (
-        <div>
-            auth
+        <div className='font-poppins bg-[#FFFFFF]'>
+            <header className='py-3 w-11/12 mx-auto'>
+                <Navbar></Navbar>
+            </header>
             <Outlet></Outlet>
         </div>
     );
