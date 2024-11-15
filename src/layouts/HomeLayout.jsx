7b8754ca48c7cdc912868b0dbec45b8f714f1af3
shoyonp <shoyonkumar143@gmail.com>
2024-11-15 10:03:41 +0600

    category button added

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import LeftNavbar from "../components/LeftNavbar";
 import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
@@ -15,7 +16,7 @@ const HomeLayout = () => {
         <Navbar></Navbar>
       </nav>
       <main className="w-11/12 mx-auto p-t-5 grid md:grid-cols-12 gap-3">
-        <aside className="left col-span-2">left number</aside>
+        <aside className="left col-span-3"><LeftNavbar></LeftNavbar></aside>
         <section className="col-span-6">main cintent</section>
         <aside className="col-span-3">rignt navbar</aside>
       </main>
